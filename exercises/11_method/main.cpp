13c8331ca8d1b896cb4ac768507fb67719cbdc37
bowman <fancycode@gmail.com>
2025-07-24 22:02:07 -0700

    fix compile of 11 in msvc

--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -15,10 +15,18 @@ struct Fibonacci {
 
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
+    #ifdef _MSC_VER
+    Fibonacci fib;
+    fib.cache[0]=0;
+    fib.cache[1]=1;
+    fib.cache[1]=1;
+    fib.cached = 3;
+    #else
     Fibonacci fib {
         .cache = {0, 1, 1}, // 初始化前两个斐波那契数
         .cached = 3      // 已缓存的斐波那契数个数
     };
+    #endif
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
